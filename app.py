6062f91ac2595a64bdcba9a37b99d64a06d0e141
AndrekaiqueDevjunior <andrekaidellisola@gmail.com>
2024-06-07 08:16:56 -0300

    Aula 12 - Excluindo Registros

--- app.py
+++ app.py
@@ -9,57 +9,95 @@ def index():
 
 @app.route("/salvar_aluno", methods=['POST'])
 def salvar_aluno():
-    data = request.get_json()
-    print(data)
-    
+    dict = request.get_json()
+            
     conn = sqlite3.connect('banco_de_dados/banco_de_dados.db')
     cursor = conn.cursor()
     
-    SQL = """INSERT INTO 
-        alunos (
-            nome,
-            sobre_nome,
-            nome_do_pai,
-            nome_da_mae,
-            data_de_nascimento,
-            telefone,
-            cpf,
-            logradouro,
-            rua,
-            bairro,
-            cidade,
-            estado,
-            cep      
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
-    
-    cursor.execute(SQL, 
-    (
-        data['nome'],
-        data['sobre_nome'],
-        data['nome_do_pai'],
-        data['nome_da_mae'],
-        data['data_de_nascimento'],
-        data['telefone'],
-        data['cpf'],
-        data['logradouro'],
-        data['rua'],
-        data['bairro'],
-        data['cidade'],
-        data['estado'],
-        data['cep']
-    ))
+    ## Quando é Alterar tem ID
+    ## Quando é Inserir não ID
+    if dict['id'] == '':        
+        SQL = """ INSERT INTO
+            alunos (
+                nome,
+                sobre_nome,
+                nome_do_pai,
+                nome_da_mae,
+                data_de_nascimento,
+                telefone,
+                cpf,
+                logradouro,
+                rua,
+                bairro,
+                cidade,
+                estado,
+                cep            
+            ) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?);"""
+        
+        cursor.execute(SQL,
+            (
+                dict['nome'],
+                dict['sobre_nome'],
+                dict['nome_do_pai'],
+                dict['nome_da_mae'],
+                dict['data_de_nascimento'],
+                dict['telefone'],
+                dict['cpf'],
+                dict['logradouro'],
+                dict['rua'],
+                dict['bairro'],
+                dict['cidade'],
+                dict['estado'],
+                dict['cep'],
+            )
+        )
+    else:
+        SQL = """ UPDATE alunos 
+                SET
+                    nome = ?,
+                    sobre_nome = ?,
+                    nome_do_pai = ?,
+                    nome_da_mae = ?,
+                    data_de_nascimento = ?,
+                    telefone = ?,
+                    cpf = ?,
+                    logradouro = ?,
+                    rua = ?,
+                    bairro = ?,
+                    cidade = ?,
+                    estado = ?,
+                    cep = ?         
+                WHERE id = ?;"""
+        cursor.execute(SQL,
+            (
+                dict['nome'],
+                dict['sobre_nome'],
+                dict['nome_do_pai'],
+                dict['nome_da_mae'],
+                dict['data_de_nascimento'],
+                dict['telefone'],
+                dict['cpf'],
+                dict['logradouro'],
+                dict['rua'],
+                dict['bairro'],
+                dict['cidade'],
+                dict['estado'],
+                dict['cep'],
+                dict['id'],
+            )
+        )
+        
     
     conn.commit()
     conn.close()
-    
     return jsonify(retorno="Sucesso")
-
+  
 @app.route("/ler_todos_alunos", methods=['POST'])
 def ler_todos_alunos():
     conn = sqlite3.connect('banco_de_dados/banco_de_dados.db')
     cursor = conn.cursor()
+    SQL = """ SELECT * FROM alunos;"""
     
-    SQL = """SELECT * FROM alunos;"""
     cursor.execute(SQL)
     dados = cursor.fetchall()
     print(dados)
@@ -67,26 +105,38 @@ def ler_todos_alunos():
     conn.close()
     return jsonify(dados=dados)
 
+@app.route("/exclui_aluno", methods=['POST'])
+def exclui_aluno():
+    dict = request.get_json()
+    conn = sqlite3.connect('banco_de_dados/banco_de_dados.db')
+    cursor = conn.cursor()
+    SQL = """ DELETE FROM alunos WHERE id=""" + str(dict['id']) + """;"""
+    cursor.execute(SQL)
+    conn.commit()
+    
+    conn.close()
+    return jsonify(x=0)
+
 @app.route("/ler_aluno_especifico", methods=['POST'])
 def ler_aluno_especifico():
     dict = request.get_json()
     conn = sqlite3.connect('banco_de_dados/banco_de_dados.db')
     cursor = conn.cursor()
-    
-    SQL = """ SELECT * FROM alunos WHERE id =""" + str(dict['id']) + """ ;""" 
+    SQL = """ SELECT * FROM alunos WHERE id=""" + str(dict['id']) + """;"""
     cursor.execute(SQL)
     dados = cursor.fetchall()
     
-    SQL = """ PRAGMA table_info(alunos);""" 
+    SQL = """ PRAGMA table_info(alunos);"""
     cursor.execute(SQL)
     cabecalho = cursor.fetchall()
     
-    
+        
     conn.close()
     return jsonify(
-    dados=dados,
-    cabecalho = cabecalho
+        dados=dados,
+        cabecalho=cabecalho
     )
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+
+app.run(debug=True, host='0.0.0.0')
--- static/js/funcoes_gerais.js
+++ static/js/funcoes_gerais.js
@@ -1,26 +1,22 @@
-function gera_input(item, parent) {
-    var row = document.createElement('div');
-    row.className = 'row mt-1';
-
-    var col = document.createElement('div');
-    col.className = 'col';
-
-    var label = document.createElement('label');
-    label.innerHTML = item.label_innerHTML;
-
-    var input = document.createElement('input');
-    input.type = item.type;  // Adicionado ponto e vírgula
-    input.name = item.name;  // Adicionado ponto e vírgula
-    input.className = 'form-control';
-    
-    // Verifica se item.input_class existe antes de adicionar a classe
-    if (item.input_class) {
-        input.classList.add(item.input_class);
-    }
-
-    col.appendChild(label);
-    col.appendChild(input);
-    row.appendChild(col);
-
-    $(parent).append(row);
+function gera_input(item, parent){
+	var row = document.createElement('div')
+	row.className = 'row mt-1'
+	
+	var col = document.createElement('div')
+	col.className = 'col'
+	
+	var label = document.createElement('label')
+	label.innerHTML = item.label_innerHTML
+	
+	var input = document.createElement('input')
+	input.type = item.type
+	input.name = item.name	
+	input.className = 'form-control'
+	input.autocomplete = 'one-time-code'
+	input.classList.add(item.input_class)
+	
+	col.appendChild(label)
+	col.appendChild(input)
+	row.appendChild(col)
+	$(parent).append(row)
 }
